379bc55999b18585680664a0fb6e840a84e743f1
MimaDiodniy <mimadiodniy@gmail.com>
2024-11-15 02:08:09 +0300

    Update task1.py

--- лабораторная работа 3/task1.py
+++ лабораторная работа 3/task1.py
@@ -1,15 +1,15 @@
 # TODO Напишите функцию для поиска индекса товара
-def search_index(products,n):
+def index(products,n):  # Объявили функцию с двумя аргументами
     if n in products:
-        return products.index(n)
+        return products.index(n)  # возвращаем индекс первого вхождения
     else:
-        return None
+        return None  # если товар не найден, возвращаем None
 
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
-for find_product in ['банан', 'груша', 'персик']:
-    index_of_product = search_index(items_list, find_product)
+for find_product in ['банан', 'груша', 'персик']:  # список искомых товаров
+    index_of_product = index(items_list, find_product)  # находим индекс товара
     if index_of_product is not None:
         print(f"Первое вхождение товара '{find_product}' имеет индекс {index_of_product}.")
     else:
