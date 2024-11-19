cc19d2c17e171badda012ce3759fc9fb723ae269
MimaDiodniy <mimadiodniy@gmail.com>
2024-11-19 03:50:48 +0300

    Update task2.py

--- Лабораторная работа 2/task2.py
+++ Лабораторная работа 2/task2.py
@@ -1,10 +1,11 @@
+import math
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months_number = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 money_capital = 0
-for i in range(months_number):
-    money_capital += spend - salary
-    spend = spend * (increase + 1)
-print(f"Подушка безопасности, чтобы протянуть {months_number} месяцев без долгов:", int(money_capital))
+for i in range(months_number):  # прогоняем цикл по каждому месяцу, количество которых равно 10
+    money_capital += spend - salary  # изменение подушки из-за разницы между зарплатой и затратами
+    spend = spend * (increase + 1)  # изменение трат с учётом ежемесячного роста цен
+print(f"Подушка безопасности, чтобы протянуть {months_number} месяцев без долгов:", math.ceil(money_capital))  # math.ceil округляет число до ближайшего целого числа в большую сторону
 
